757911b2796ed204e692e09a02fb07cd8fc4ed2c
Walt WGA <vladmikhov@gmail.com>
2022-09-10 23:36:06 +0300

    added fe part with nuxt

--- back/index.js
+++ back/index.js
@@ -3,14 +3,15 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const http = require('http');
 const PORT = 3000;
-const { routes } = require('./src/routes')
+const { routes } = require('./src/routes');
+const cors = require('cors');
 
 mongoose.connect(
     'mongodb://localhost:27017/menv',
 );
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
