99da23640bc701f35c0e5e295ba2eb8d7278acdd
Walt WGA <vladmikhov@gmail.com>
2022-09-12 00:13:00 +0300

    added ssr

---
+++ client/pages/doctor/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Doctor {{ doctor.name }}</h1>
+    <h3>my number is {{ doctor._id }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const response = await fetch(`http://localhost:3000/api/v1/doctors/${params.id}`);
+    const doctor = await response.json();
+
+    return {
+      doctor
+    }
+  },
+
+  data () {
+    return {
+      doctor: null,
+    }
+  }
+}
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,61 @@
 <template>
-  <Tutorial />
+  <div class="flex">
+    <div
+      class="card-doctor"
+      v-for="doctor in doctors"
+      :key="doctor.id"
+      @click="$router.push(`/doctor/${doctor._id}`)">
+      <img :src="doctor.imageUrl" alt="" class="card-doctor__image">
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  async asyncData() {
+    const response = await fetch('http://localhost:3000/api/v1/doctors');
+    const doctors = await response.json();
+
+    return {
+      doctors
+    }
+  },
+
+  data() {
+    return {
+      doctors: null,
+    }
+  },
 })
 </script>
+
+<style scoped>
+  .flex {
+    display: flex;
+    column-gap: 15px;
+  }
+
+  .card-doctor {
+    width: 150px;
+    height: 230px;
+    border-radius: 15px;
+    border: 2px solid red;
+    overflow: hidden;
+    cursor: pointer;
+    transition: all 0.3s;
+  }
+
+  .card-doctor:hover {
+    transform: scale(1.1);
+    transition: all 0.3s;
+  }
+
+  .card-doctor__image {
+    width: 100%;
+    height: 100%;
+  }
+</style>
